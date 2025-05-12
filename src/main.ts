f5e4903ead4963a6c26c6e5b1e3da5e13febcfba
Ricky Ultimate <rickyogigi@gmail.com>
2025-05-12 12:35:20 +0100

    chore: configure swagger

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,20 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { CloudStorageFilter } from './filters/cloud-storage.filter';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const config = new DocumentBuilder()
+    .setTitle('Multi-Cloud Storage API')
+    .setDescription('API for managing files across multiple cloud providers')
+    .setVersion('1.0')
+    .addTag('storage')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
   app.useGlobalFilters(new CloudStorageFilter());
   await app.listen(3000);
 }
